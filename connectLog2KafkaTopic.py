a8cde695a4c56189a2938e5d68361aed49b6fbec
yuvaraj228 <85035514+yuvaraj228@users.noreply.github.com>
2023-05-30 23:28:28 -0400

    Update connectLog2KafkaTopic.py

--- connectLog2KafkaTopic.py
+++ connectLog2KafkaTopic.py
@@ -1,17 +1,17 @@
 import json
 import re
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 
 # Configure Kafka connection
-bootstrap_servers = 'https://nldkfksql-001.edj.devjones.com:8083'
+bootstrap_servers = 'https://nldkfkbrkr-001.edj.devjones.com:8083,https://nldkfkbrkr-002.edj.devjones.com:8083'
 topic = 'test'
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+producer = Producer({'bootstrap_servers': bootstrap_servers})
 
 # Define log file path
-log_file = 'path_to_your_log_file'
+log_file = '/Users/sdendi/IdeaProjects/src/connect.log'
 
 # Define log pattern using regular expression
-log_pattern = r'^\[([^\]]+)\] \[(DEBUG|INFO|WARN|ERROR)\] \[([^\]]+)\] (.*)$'
+log_pattern = r'^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3})\] (\w+) \[([^\]]+)\] (.*)$'
 
 # Read log file
 with open(log_file, 'r') as file:
@@ -31,10 +31,10 @@ with open(log_file, 'r') as file:
 
             # Convert JSON object to string
             log_data_str = json.dumps(log_data)
-
+            print(log_data_str)
             # Publish log data to Kafka topic
-            producer.send(topic, value=log_data_str.encode())
-
-# Flush and close the Kafka producer
+            producer.produce(topic, value=log_data_str.encode())
+#
+# # Flush and close the Kafka producer
 producer.flush()
 producer.close()
